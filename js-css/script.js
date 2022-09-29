68ff91f35c9551c67d7a41fd9005736019a373ed
hanif ramadhani <69223521+haniframadhani@users.noreply.github.com>
2022-09-29 15:19:04 +0700

    tambah tanggal

--- js-css/script.js
+++ js-css/script.js
@@ -1,8 +1,92 @@
 const display = document.getElementById('clock');
+const kalender = document.querySelector('.date');
 const audio = new Audio('js-css/alarm/airraidsiren.mp3');
 audio.loop = true;
 let alarmTime = null;
 let alarmTimeout = null;
+
+function tanggalHariIni() {
+	let tanggal = new Date();
+	let day = tanggal.getDay();
+	let date = tanggal.getDate();
+	let month = tanggal.getMonth();
+	let year = tanggal.getFullYear();
+	let hari = '';
+	let bulan = '';
+	let tanggalFull = '';
+
+	switch (day) {
+		case 0:
+			hari = 'Minggu,';
+			break;
+		case 1:
+			hari = 'Senin,';
+			break;
+		case 2:
+			hari = 'Selasa,';
+			break;
+		case 3:
+			hari = 'Rabu,';
+			break;
+		case 4:
+			hari = 'Kamis,';
+			break;
+		case 5:
+			hari = 'Jumat,';
+			break;
+		case 6:
+			hari = 'Sabtu,';
+			break;
+	}
+
+	switch (month) {
+		case 0:
+			bulan = 'Januari';
+			break;
+		case 1:
+			bulan = 'Februari';
+			break;
+		case 2:
+			bulan = 'Maret';
+			break;
+		case 3:
+			bulan = 'April';
+			break;
+		case 4:
+			bulan = 'Mei';
+			break;
+		case 5:
+			bulan = 'Juni';
+			break;
+		case 6:
+			bulan = 'Juli';
+			break;
+		case 7:
+			bulan = 'Agustus';
+			break;
+		case 8:
+			bulan = 'September';
+			break;
+		case 9:
+			bulan = 'Oktober';
+			break;
+		case 10:
+			bulan = 'November';
+			break;
+		case 11:
+			bulan = 'Desember';
+			break;
+	}
+
+	tanggalFull = [hari, date, bulan, year];
+	tanggalFull = tanggalFull.join(" ");
+	kalender.innerHTML = tanggalFull;
+}
+
+tanggalHariIni();
+
+setInterval(() => tanggalHariIni(), 60000);
+
 function dateToText(date) {
 	var hours = date.getHours()
 	var minutes = date.getMinutes();
@@ -12,6 +96,7 @@ function dateToText(date) {
 	if (hours < 10) hours = '0' + hours;
 	return hours + ":" + minutes + ":" + seconds;
 }
+
 function updateClocks() {
 	for (var i = 0; i < window.arrClocks.length; i++) {
 		var clock = window.arrClocks[i];
@@ -19,6 +104,7 @@ function updateClocks() {
 		clock.innerHTML = dateToText(new Date(new Date().getTime() + offset));
 	}
 }
+
 function startClocks() {
 	clockElements = document.getElementsByClassName('clock');
 	window.arrClocks = []
@@ -37,6 +123,7 @@ function startClocks() {
 	updateClocks();
 	clockID = setInterval(updateClocks, 1000);
 }
+
 function setAlarmTime(value) {
 	alarmTime = value;
 }
